180b2583327f184dbe2e3698d292993aaffe609a
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-11-14 19:40:26 +0100

    Add files via upload

---
+++ recenicaToRijeci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recenicaToRijeci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica;
+
+            Console.Write("Napisi neku recenicu --> ");
+            recenica = Convert.ToString(Console.ReadLine());
+
+            string[] rijeci = recenica.Split(' ');
+            Console.Write(rijeci.First() + " " + rijeci.Last());
+
+            Console.ReadKey();
+        }
+    }
+}
